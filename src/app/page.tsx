1064a14fd0e35d066a5b4d6eb8e2c0a22e7d982d
smilingpet <palkin@smilingpets.in>
2026-09-17 12:17:59 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default async function HomePage() {
           title="Hand Picked Exclusive Pet Products"
           icon={DogIcon}
           products={exclusiveProducts.items}
-          viewAllHref="/collections/all"
+          viewAllhref="/search"
         />
       </RenderErrorBoundary>
 
